d93a7e6fb2c73383c9e22700c77b695d246d7d17
ELMESKINEAnas <78366209+ELMESKINEAnas@users.noreply.github.com>
2022-02-24 16:17:52 +0100

    dash

---
+++ material-dashboard-react/src/layouts/Steps/Condition1.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import Multistep from './MultiStep';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function AlertDialogSlide(props) {
+
+const{handleClose,open}=props;
+
+
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+        <MultiStep />
+          <DialogContentText id="alert-dialog-slide-description">
+            yoyo
+          </DialogContentText>
+        </DialogContent>
+        
+      </Dialog>
+    </div>
+  );
+}
